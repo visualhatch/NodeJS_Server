10c88cc7af9ca51d2e77072820c9e567c031ea5a
visualhatch <rujpYf-wujcur-taqxo6>
2019-01-03 20:57:46 -0600

    EOD - 1-3-19

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const script2 = require('./script2.js');
 
 const a = script2.largeNumber;
-const b = 5;
+const b = 1;
 
 console.log(a + b);
+
+
+// // const script2 = require('fs');
+// //
+// // console.log(script2);
+//
+// // const script2 = require('http');
+// //
+// // console.log(script2);
+//
+// const script2 = require('fs');
+//
+// console.log(script2);
